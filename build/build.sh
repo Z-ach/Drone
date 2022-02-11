3a00315aea94e21d46187937b3312b412cd43abb
z-ach <zachkaufman97@gmail.com>
2022-02-11 14:08:35 -0800

    build system improvements

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,28 @@
+#!/usr/bin/env bash
+
+# Get dir name where script lives
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Define dockcross compilation helper script, build/src dirs
+DOCKCROSS="$DIR/beagle_armv7"
+BUILD="build"
+SRC="src"
+
+# Move into build directory and clean cmake/make files
 cd $DIR
 rm -r CMake*
 rm cmake*
 rm Makefile
-cmake ../src
-make
-mkdir -p ../bin
-mv Drone ../bin
+
+# Important - must cd to root proj dir, otherwise dockcross helper script
+# will not have access to relevant directories
+cd ..
+
+# Run dockcross script with cmake, then make
+$DOCKCROSS cmake -S$SRC -B$BUILD
+$DOCKCROSS make -C$BUILD
+
+if [ "$1" == "-d" ]; then
+    # If deploy flag was set, rsync compiled binary to drone
+    rsync bin/Drone bbb:~/Drone/bin/
+fi
