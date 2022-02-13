aa4c76ae2bec2fe3dbda654f95f1ba5b2cefa012
z-ach <zachkaufman97@gmail.com>
2022-02-13 07:46:06 -0800

    docker update: pull latest version and don't rm image

--- resources/install_beagle_compiler.sh
+++ resources/install_beagle_compiler.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Load docker tar into image
-docker build -t zachkaufman/beagle_armv7 .
+docker build --pull -t zachkaufman/beagle_armv7 .
 
 # Run and output dockcross helper script
-docker run --rm zachkaufman/beagle_armv7 > beagle_armv7
+docker run zachkaufman/beagle_armv7 > beagle_armv7
 
 # Allow execute permissions
 chmod +x beagle_armv7
